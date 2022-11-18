b6d60cfb9c0f1e316ac262058bc44611fd74a971
Bhalesuyog94 <113917613+Bhalesuyog94@users.noreply.github.com>
2022-11-19 02:02:36 +0530

    Add files via upload

---
+++ LinkList/LinkList/LinkLists.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinkList
+{
+    internal class LinkLists
+    {
+        internal Node head; //new 
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            Node temp = head;
+            if (position < 1)
+            {
+                Console.WriteLine("Invalid position");
+
+            }
+            if (position == 1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                while (position != 0)
+                {
+
+                    if (position == 1)
+                    {
+                        var node = new Node(data);
+                        node.next = temp.next;
+                        temp.next = node;
+                        break;
+                    }
+                    temp = temp.next;
+                }
+                if (position != 1)
+                {
+                    Console.WriteLine("Position out of range");
+                }
+            }
+            return head;
+        }
+
+        internal Node RemoveFirstNode()
+        {
+            if (this.head == null)
+                return null;
+            this.head = this.head.next;
+            return this.head;
+        }
+
+        internal Node RemovaLastNode()
+        {
+            if (head == null)
+                return null;
+            if (head.next == null)
+                return null;
+            Node newNode = head;
+            while (newNode.next.next != null)
+            {
+                newNode = newNode.next;
+            }
+            newNode.next = null;
+            return head;
+        }
+        internal void Display()
+        {
+            Console.WriteLine("");
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is Empty");
+                return;
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.Write(" " + temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
+
+        internal Node Search(int value)
+        {
+            while (this.head != null)
+            {
+                if (this.head.data == value)
+                {
+                    return this.head;
+                }
+                this.head = this.head.next;
+            }
+            return null;
+        }
+
+    }
+}
---
+++ LinkList/LinkList/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LinkList;
+using System.Collections.Generic;
+Console.WriteLine("===============");
+LinkLists link = new LinkLists();
+link.Add(56);
+link.Add(30);
+link.Add(70);
+//link.RemoveFirstNode();
+//link.RemovaLastNode();
+//link.Search(30);
+//link.Display();
+link.InsertAtParticularPosition(3, 40);
+link.Display();
